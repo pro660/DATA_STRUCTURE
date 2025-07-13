31cc44b40041d993faf3fb13e427deb0f92e5e6e
pro660 <gw904202@gmail.com>
2025-07-13 17:31:50 +0900

    문자 함수 제작

---
+++ DIYfunction.cpp
@@ -0,0 +1,63 @@
+// #include <stdio.h>
+// /*char *strcpy(char *dest, char *src){
+	
+// 	char *start=dest;
+	
+// 	while(*src!='\0'){
+// 		*dest=*src;
+// 		dest++;
+// 		src++;
+// 	}
+// 	*dest = '\0';
+// 	return start;
+// }
+// */
+
+// /*int strlen(char *src){
+// 	int count=0;
+	
+// 	while(*src!='\0'){
+// 		count++;
+// 		src++;
+// 	}	
+	
+// 	return count;
+// }
+// */
+
+// /*int strcmp(char *dest, char *src){
+	
+// 	while(*dest!='\0' && *src!='\0'){
+// 		if(*dest>*src)
+// 			return 1;
+// 		else if(*dest<*src)
+// 			return -1;
+// 		else{
+// 			dest++;
+// 			src++;
+// 		}
+// 	}	
+// 	return 0;
+// }
+// */
+
+// int main(){
+// 	char dest[10]="banana";
+// 	char src[10]="banana";
+	
+// 	//strcpy(dest,src);
+// 	//int c=strlen(src);
+// 	strcmp(dest,src);
+	
+// 	//printf("%s",dest);
+// 	//printf("%d",c);
+// 	if(strcmp(dest,src)==0)
+// 		printf("���ڿ��� ����");
+// 	else
+// 		printf("���ڿ��� ���� �ʴ�.");
+	
+	
+// 	return 0;
+// }
+
+
